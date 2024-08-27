f1eb933e944f6d7aac629e8e471ee91ab7878751
Jaden Ong <ong.jadengabriel@gmail.com>
2024-08-27 12:05:40 -0700

    progress update
    creating the board and different colors for the two players, including css styles for height, width, colors, etc.
    
    also note that the square element is both a part of beige and square class

--- chessGameProject/app.js
+++ chessGameProject/app.js
@@ -7,8 +7,26 @@ const infoDisplay = document.querySelector("#info-display");
 
 const width = 8; // to define a 8x8 board below
 
-const startPieces = [
-
-
+const startPieces = [ // think of an array of 64 items
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, queen, king, bishop, knight, rook,
     
 ]
+
+
+function createBoard(){
+    startPieces.forEach((startPiece) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.classList.add('beige')
+        gameBoard.append(square)
+    })
+}
+
+createBoard()
