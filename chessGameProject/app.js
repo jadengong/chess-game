0fa765231c476d44a9e77e8807e7ca1cc12f981f
Jaden Ong <ong.jadengabriel@gmail.com>
2024-08-27 17:21:35 -0700

    game progress
    each individual square was given an id and tag so that it can be easily identified. each square was also given alternating colors green and white (for now) with an if else statement to make sure that at the end of one row, the color that ends also starts the next so that it resembles a chess board.

--- chessGameProject/app.js
+++ chessGameProject/app.js
@@ -21,12 +21,23 @@ const startPieces = [ // think of an array of 64 items
 
 
 function createBoard(){
-    startPieces.forEach((startPiece) => {
+    startPieces.forEach((startPiece, i) => {
         const square = document.createElement('div')
         square.classList.add('square')
-        square.classList.add('beige')
+        square.innerHTML = startPiece // save each piece as its svg file
+        square.setAttribute('square-id', i) // unique attribute for square so that each is ID'd.
+       // square.classList.add('beige')
+       const row = Math.floor((63 - i) / 8) + 1 // define the row we are in, 8x8 board 
+
+        if(row % 2 === 0){
+            square.classList.add(i % 2 === 0 ? "green" : "white")
+        }else {
+            square.classList.add(i % 2 === 0 ? "white" : "green") // has to be if else so that at the end of a row, if a square ends on one color, the first square in the next row has to be the same color.
+        }
+
+
         gameBoard.append(square)
     })
 }
 
-createBoard()
+createBoard();
