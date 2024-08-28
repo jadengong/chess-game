de0458456dea5dd42103dc1e4b52a153fb15cb72
Jaden Ong <ong.jadengabriel@gmail.com>
2024-08-27 19:06:01 -0700

    color finalization and drag and drop progress
    finalized the coloring schemes of the pieces and gameboard as well as begin the process for taking the id of an element (piece) that is identified by clicking it. the squares are added with an eventlistener so that they can call a function when it happens

--- chessGameProject/app.js
+++ chessGameProject/app.js
@@ -25,14 +25,25 @@ function createBoard(){
         const square = document.createElement('div')
         square.classList.add('square')
         square.innerHTML = startPiece // save each piece as its svg file
+
+        // make each draggable
+        square.firstChild?.setAttribute('draggable', true) // if a square has a first child (it has a piece in the slot), give it the attribute draggable found (checked by the question mark)
+
         square.setAttribute('square-id', i) // unique attribute for square so that each is ID'd.
-       // square.classList.add('beige')
-       const row = Math.floor((63 - i) / 8) + 1 // define the row we are in, 8x8 board 
+
+
+        const row = Math.floor((63 - i) / 8) + 1 // define the row we are in, 8x8 board 
 
         if(row % 2 === 0){
             square.classList.add(i % 2 === 0 ? "green" : "white")
         }else {
             square.classList.add(i % 2 === 0 ? "white" : "green") // has to be if else so that at the end of a row, if a square ends on one color, the first square in the next row has to be the same color.
+        }
+
+        if(i <= 15){ // we want the first 16 squares to be black (team 1); enter the square, enter the child of the square, and make classList black. 
+            square.firstChild.firstChild.classList.add('brown')
+        }else if(i >= 48){
+            square.firstChild.firstChild.classList.add('black')
         }
 
 
@@ -41,3 +52,40 @@ function createBoard(){
 }
 
 createBoard();
+
+
+
+const allSquares = document.querySelectorAll("#gameboard .square")
+
+allSquares.forEach(square => {
+    square.addEventListener('dragstart', dragStart) // square will be looking for a dragstart event, and when it starts a function dragStart will be called
+    square.addEventListener('dragover', dragOver) // same for dragging over
+    square.addEventListener('drop', dragDrop) // and for dropping it in a new square OR a square with a piece in it
+
+})
+
+let startPositionId
+let draggedElement
+
+function dragStart(e){
+    startPositionId = (e.target.parentNode.getAttribute('square-id')) // mark the object with target to have only its target location show up on console ;; just gets us the id of the piece we are trying to drag!!
+    draggedElement = e.target
+}
+
+function dragOver(e){
+    e.preventDefault() // prevents any funky behavior since without this, it would tell me what im dragging over
+}
+
+function dragDrop(e){
+    e.stopPropagation() // prevents any more funky behavior like calling function twice
+
+    const taken = e.target.classList.contains('piece') // if target contains the class piece, then clearly its a piece
+
+    e.target.parentNode.append(draggedElement) // append the target element to square with a piece already in it, then remove it
+    e.target.remove() 
+
+    // now we have to make sure that this only happens if there is a piece, and if it is an opponents piece
+
+
+
+}
